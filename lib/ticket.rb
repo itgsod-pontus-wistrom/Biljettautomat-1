2d6b759619ecea62cf78d834748e29baa2431eae
itgsod-pontus-wistrom <pontus.wistrom@elev.it-gymnasiet.se>
2015-10-14 11:49:28 +0200

    Fixad uppgift

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,17 +1,17 @@
 # ask age is a function that asks the person how old they are
 def ask_age()
-  puts "How old are you?"
+  puts "Enter your age"
   age=gets.to_i
   return age
 end
 
 def ticket_price(age)
   if age < 18
-    return "10kr"
+    return 10
   elsif age > 64
-    return "15kr"
+    return 15
   end
-  return "20kr"
+  return 20
 end
 
 puts ticket_price(ask_age)
